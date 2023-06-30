800588f0c94cc904d0c96dd44f0d6a19886ef16f
Şaban Dönmez <eren.donmez.11@gmail.com>
2023-06-30 19:45:20 +0300

    Receiving Data and Model Validations

--- BtkAkademi/Controllers/CourseController.cs
+++ BtkAkademi/Controllers/CourseController.cs
@@ -7,7 +7,8 @@ namespace BtkAkademi.Controllers
  { 
     public IActionResult Index()
     {
-        return View();
+        var model = Repository.Applications;
+        return View(model);
     }
     public IActionResult Apply()
     {
@@ -17,8 +18,17 @@ namespace BtkAkademi.Controllers
     [ValidateAntiForgeryToken]
     public IActionResult Apply( [FromForm] Candidate model)
     {
-        Repository.Add(model);
-        return View("Feedback",model);
+        if (Repository.Applications.Any(c=>c.Email.Equals(model.Email)))
+        {
+            ModelState.AddModelError("","There is already an application for you.");
+        }
+        if (ModelState.IsValid)
+        {
+            Repository.Add(model);
+            return View("Feedback",model);
+        }
+        return View();
+        
     }
  }  
 }
